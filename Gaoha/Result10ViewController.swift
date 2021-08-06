c57f9e85e6f2cc567d471231c4fb56d9917ce3b7
Takai Yuzuki <youyuegaojing 13@gmail.com>
2021-08-06 19:56:02 +0900

    あ

---
+++ Gaoha/Result10ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  Result10ViewController.swift
+//  Gaoha
+//
+//  Created by 高井佑月 on 2021/08/06.
+//
+
+import UIKit
+
+class Result10ViewController: UIViewController {
+    
+    @IBAction func back() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("hoge")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
